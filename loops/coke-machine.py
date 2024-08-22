39c6c846331dc80df0c1ef635a77b1a1f78ccdde
stautuan <stautuan89@gmail.com>
2024-08-21 20:41:10 -0400

    refactor

--- loops/coke-machine.py
+++ loops/coke-machine.py
@@ -1,5 +1,9 @@
 """
-Implement a program that prompts the user to insert a coin, one at a time, each time informing the user of the amount due. Once the user has inputted at least 50 cents, output how many cents in change the user is owed. Assume that the user will only input integers, and ignore any integer that isn't an accepted denomination.
+Implement a program that prompts the user to insert a coin, one at a 
+time, each time informing the user of the amount due. Once the user 
+has inputted at least 50 cents, output how many cents in change the 
+user is owed. Assume that the user will only input integers, and ignore 
+any integer that isn't an accepted denomination.
 """
 
 amount_due = 50
@@ -16,4 +20,4 @@ while amount_due > 0:
         else:
             print(f"Amount Due: {amount_due}")
     else:
-        print("Amount Due: 50")
+        print(f"Amount Due: {amount_due}")
