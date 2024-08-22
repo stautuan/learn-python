0f88b699e23a3b7dac2032aa39922bfdadff0742
stautuan <stautuan89@gmail.com>
2024-08-21 20:26:56 -0400

    update file

--- loops/coke-machine.py
+++ loops/coke-machine.py
@@ -1,3 +1,7 @@
+"""
+Implement a program that prompts the user to insert a coin, one at a time, each time informing the user of the amount due. Once the user has inputted at least 50 cents, output how many cents in change the user is owed. Assume that the user will only input integers, and ignore any integer that isn't an accepted denomination.
+"""
+
 amount_due = 50
 print(f"Amount Due: {amount_due}")
 
