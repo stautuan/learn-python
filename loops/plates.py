0148cb83a7e14b06efa168a9bb4c6842d519b851
stautuan <stautuan89@gmail.com>
2024-08-23 12:41:01 -0400

    fix typo

--- loops/plates.py
+++ loops/plates.py
@@ -1,9 +1,9 @@
 """
 Implement a program that prompts the user for a vanity plate and then
 output Valid if meets all of the requirements or Invalid if it does not.
-Assume that any letters in the user’s input will be uppercase. Structure
+Assume that any letters in the user's input will be uppercase. Structure
 your program per the below, wherein is_valid returns True if s meets all
-requirements and False if it does not. Assume that s will be a str. You’re
+requirements and False if it does not. Assume that s will be a str. You're
 welcome to implement additional functions for is_valid to call (e.g., one
 function per requirement).
 """
