b5123c041dd5c159c5341035bb9075668f9326d5
stautuan <stautuan89@gmail.com>
2024-09-05 16:08:19 -0400

    professor

---
+++ 4-libraries/professor.py
@@ -0,0 +1,24 @@
+"""
+Implement a program that:
+    - Prompts the user for a level, n. 
+    - If user does not input 1, 2, or 3, prompt again.
+  - Randomly generates 10 math problems formatted as `X + Y = `,  wherein each of X and Y is a non-negative
+  integer with n digits. No need to support operations other than addition (+).
+  - Prompts the user to solve each of those problems. If an answer is not correct (or not even a number), 
+  the program should output `EEE` and prompt the user again, allowing the user up to three tries in total
+  for that problem. If the user has still not answered correctly after three tries, the program should output
+  the correct answer.
+  - The program should ultimately output the user's score: the number of correct answer out of 10.
+"""
+
+# prompt user for level
+    # if level == 1 or 2 or 3, generate math problems
+    # else prompt the user again
+
+
+def main():
+
+
+
+if __name__ == "__main__":
+	main()
