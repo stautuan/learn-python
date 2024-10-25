e108c84eb3447b243976b58a67aa6d850ce7ab43
stautuan <stautuan89@gmail.com>
2024-10-25 13:09:59 -0400

    shirtificate

---
+++ 8-oop/shirtificate/shirtificate.py
@@ -0,0 +1,17 @@
+"""
+Implement a program that prompts the user for their name and outputs,
+using fpdf2, a CS50 shirtificate in a file called `shirtificate.pdf`
+similar to this one for John Harvard, with these specifications:
+- the orientation of the pdf should be Portrait.
+- the format of the pdf should be A4 (210mm wide by 297mm tall).
+- the top of the pdf should say "CS50 Shirtificate" as text, centered horizontally.
+- the shirt's image should be centered horizontally.
+- the user's name should be on top of the shirt in white text.
+"""
+
+def main():
+    print(input("Name: "))
+
+
+if __name__ == "__main__":
+    main()
